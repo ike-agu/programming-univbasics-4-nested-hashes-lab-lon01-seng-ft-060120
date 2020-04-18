b331d41d2023cdca460f217476ef35f4aefcee6c
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-18 23:22:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -140,5 +140,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages][0]="Assembly"
+programmer_hash[:dennis_ritchie][:languages]="Assembly"
 end
