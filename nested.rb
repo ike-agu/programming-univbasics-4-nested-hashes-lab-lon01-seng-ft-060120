58721aa3ac5794aa799ad93fa70746f30aed18bb
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-18 21:48:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,9 +14,14 @@ def hopper
         :known_for => "Unix",
         :languages => ["C"]
       }
+      
+      
+      programmer_hash[:grace_hopper]
     }
 
 end
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
