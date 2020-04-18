edea9dac940b0b062844cdc8ddf4a7dae474a9cf
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-18 22:30:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,14 +86,10 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
-      
-      
     }
-   programmer_hash[:yukihiro_matsumoto] <<  {:known_for => "Ruby",:languages => ["LISP" , "C"]}
-   
-   
-   #programmer_hash[:yukihiro_matsumoto] <<  {:known_for => "Ruby"}
-  #programmer_hash[:yukihiro_matsumoto] << {:languages => ["LISP" , "C"]}
+   programmer_hash[] << {:yukihiro_matsumoto =>{}}
+   programmer_hash[:yukihiro_matsumoto] <<  {:known_for => "Ruby"}
+  programmer_hash[:yukihiro_matsumoto] << {:languages => ["LISP" , "C"]}
     
     programmer_hash
    
